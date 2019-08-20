f902add3fd8167241df5ca18371963bfa8577faf
relipa-dev <relipa-dev@relipasoft.com>
2019-08-20 11:40:20 +0700

    add glossary

--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -1,2 +1,11 @@
 class Glossary < ApplicationRecord
+  ALPHABET = ('a'..'z').to_a.insert(0, '#')
+
+  scope :order_alphabet, -> (char) {
+    if char == '#'
+      where("title regexp '^[0-9]'")
+    else
+      where("title like ?", "#{char}%")
+    end
+  }
 end
