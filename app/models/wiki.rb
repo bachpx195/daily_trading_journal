881ab4c3f533cd4d51d5e4917435eb1eaddd54ce
bachpx <bachpx@relipasoft.com>
2019-09-28 13:43:34 +0700

    update 28/9

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,13 +3,13 @@ class Wiki < ApplicationRecord
   belongs_to :tag
 
   scope :crypto_and_common, -> {
-    where(market_type: [0, 3])
-  }
-  
-  scope :forex_and_common, -> {
     where(market_type: [1, 3])
   }
   
+  scope :forex_and_common, -> {
+    where(market_type: [0, 3])
+  }
+  
   mount_uploader :image, ImageUploader
-  enum market_type: {crypto: 0, forex: 1, gold: 2, common: 3}
+  enum market_type: {forex: 0, crypto: 1, gold: 2, common: 3}
 end
