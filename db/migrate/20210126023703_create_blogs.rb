ff0de4c56cc1c2677fad72090676b619cb512352
bachpx195 <bachpx195@gmail.com>
2021-01-28 20:00:53 +0700

    wk

---
+++ db/migrate/20210126023703_create_blogs.rb
@@ -0,0 +1,13 @@
+class CreateBlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.datetime :public_time
+      t.integer :public_status, null: false, default: 0
+      t.string :intro_image
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
