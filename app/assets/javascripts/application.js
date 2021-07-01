13ca2b54f41a3c2e7012e0e44e4ea1c740e4abed
bachpx195 <bachpx195@gmail.com>
2021-07-02 06:31:00 +0700

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,27 +23,38 @@
 
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
+  var simplemde = new SimpleMDE({ element: document.getElementById("markdown"), tabSize: 1 });
+  $('.date-picker').change(function(){
+  });
+  if ($('textarea').length > 0) {
+    CKEDITOR.config.height = 500;
+    var data = $('.ckeditor');
+    $.each(data, function(i) {
+      CKEDITOR.replace(data[i].id)
+    });
+  }
   $('#dataTable').DataTable();
     /*!
-        * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
-        * Copyright 2013-2020 Start Bootstrap
-        * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-        */
-        (function($) {
-        "use strict";
+      * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
+      * Copyright 2013-2020 Start Bootstrap
+      * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+      */
+      (function($) {
+      "use strict";
 
-        // Add active state to sidbar nav links
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-                if (this.href === path) {
-                    $(this).addClass("active");
-                }
-            });
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this.href === path) {
+                  $(this).addClass("active");
+              }
+          });
 
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
-        });
-    })(jQuery);
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+
 });
