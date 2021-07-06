000b2f05ec6b0fd52c9241c3da88d0afa678a43d
BachPX7286 <bachpx195@gmail.com>
2021-07-06 09:47:32 +0700

    delete trash file

--- app/models/currency_pair.rb
+++
@@ -1,9 +0,0 @@
-class CurrencyPair < ApplicationRecord
-  has_many :comments, as: :commentable
-  has_many :trades
-  belongs_to :tag
-  belongs_to :base, class_name: "Merchandise"
-  belongs_to :quote, class_name: "Merchandise"
-
-  enum is_follow: {unfollow: 0, followed: 1}
-end
