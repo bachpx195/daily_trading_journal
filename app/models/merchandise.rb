fe826a10b5f49c64a4700b68d4ae12987d963546
bachpx195 <bachpx195@gmail.com>
2021-07-19 23:29:48 +0700

    update

--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -1,5 +1,5 @@
 class Merchandise < ApplicationRecord
   has_many :comments, as: :commentable
-
+  has_many :merchandise_rates, -> (object) { where("select * from merchandise_rates where merchandise_rates.base_id = ?", object.id) }, class_name: "MerchandiseRate"
   belongs_to :tag
 end
