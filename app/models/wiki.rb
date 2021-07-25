246a3955273e6b98a0351b59227ec1169f803e20
bachpx195 <bachpx195@gmail.com>
2021-07-25 15:54:01 +0700

    add bootstrap

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -5,11 +5,11 @@ class Wiki < ApplicationRecord
   scope :crypto_and_common, -> {
     where(market_type: [1, 3])
   }
-  
+
   scope :forex_and_common, -> {
     where(market_type: [0, 3])
   }
-  
+
   mount_uploader :image, ImageUploader
-  enum market_type: {forex: 0, crypto: 1, gold: 2, common: 3}
+  enum market_type: {common:0, forex: 1, crypto: 2, commodity: 3}
 end
