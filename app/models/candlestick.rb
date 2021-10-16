d765df437825cb3f69d46450cc7008b4774100e8
Bachpx <bachpx195@gmail.com>
2021-10-16 08:35:25 +0700

    update

--- app/models/candlestick.rb
+++ app/models/candlestick.rb
@@ -1,5 +1,11 @@
 class Candlestick < ApplicationRecord
   belongs_to :merchandise_rate
-
   enum time_type: {day: 1, week: 2, month: 3, hour: 4}
+
+  class << self
+    def delete_duplicate
+      Candlestick.where.not(id: Candlestick.group(:date, :time_type, :merchandise_rate_id).select("min(id)")).destroy_al
+    end
+  end
+
 end
