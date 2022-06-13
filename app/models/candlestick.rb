3e6eb8d44780ff2a1df5c98e710acef51e184779
Bachpx <bachpx195@gmail.com>
2022-06-13 20:47:31 +0700

    update

--- app/models/candlestick.rb
+++ app/models/candlestick.rb
@@ -2,6 +2,9 @@ class Candlestick < ApplicationRecord
   belongs_to :merchandise_rate
   enum time_type: {day: 1, week: 2, month: 3, hour: 4, m15: 5}
 
+  # date_between( "2022-05-10","2022-05-12")
+  scope :date_between, lambda {|start_date, end_date| where("date >= ? AND date <= ?", start_date, end_date )}
+
   class << self
     def delete_duplicate
       Candlestick.where.not(id: Candlestick.group(:date, :time_type, :merchandise_rate_id).select("min(id)")).destroy_all
