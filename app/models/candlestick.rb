4deb1ac88e8d43a53b65854044cf0cb0f5c63e8d
Bachpx-ponos <bachpx@ponos-tech.com>
2023-05-21 00:12:28 +0700

    update

--- app/models/candlestick.rb
+++ app/models/candlestick.rb
@@ -10,6 +10,10 @@ class Candlestick < ApplicationRecord
     where("date >= ? AND date <= ?", start_date.to_date.beginning_of_day, end_date.to_date.end_of_day )
   end
 
+  scope :time_between, -> start_time, end_time do
+    where("date >= ? AND date <= ?", start_time, end_time )
+  end
+
   scope :find_by_merchandise_rate, -> merchandise_rate_id, time_type, limit do
     where(merchandise_rate_id: merchandise_rate_id)
     .where(time_type: time_type)
@@ -28,18 +32,18 @@ class Candlestick < ApplicationRecord
     end
 
     # Mục đích là tìm 100 giá trị trước và sau date được chọn
-    def range_between_date date, type
+    def range_between_date date, type, limit
       case type
       when time_types["day"]
-        [date - 100.days, date + 100.days, date + 1.days]
+        [date - 100.days, date + limit.days, date + 1.days]
       when time_types["week"]
-        [date - 100.weeks, date + 100.weeks, date + 1.weeks]
+        [date - 100.weeks, date + limit.weeks, date + 1.weeks]
       when time_types["month"]
-        [date - 100.months, date + 100.months, date + 1.months]
+        [date - 100.months, date + limit.months, date + 1.months]
       when time_types["hour"]
-        [date - 100.hours, date + 100.hours, date + 1.hours]
+        [date - 100.hours, date + limit.hours, date + 1.hours]
       when time_types["m15"]
-        [date - (100*15).minutes, date + (100*15).minutes, date + 15.minutes]
+        [date - (100*15).minutes, date + (limit*15).minutes, date + 15.minutes]
       end
     end
   end
